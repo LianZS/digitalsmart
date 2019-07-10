72e5f805c430902454423a1f6dc32230ee3f88db
LianZS <2214057556@qq.com>
2019-07-11 00:38:19 +0800

    景区实时人流模版终极版

--- digitalsmart/attractions/models.py
+++ digitalsmart/attractions/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+
+
 class Scenceflow(models.Model):
     name = models.CharField(max_length=64)
     scenceid = models.IntegerField()
-    datetime = models.DateTimeField()
+    date = models.DateField()
+    detailtime = models.CharField(max_length=32)
+
     num = models.IntegerField()
     class Meta:
         db_table = "scenceflow"
-        unique_together=[['scenceid','datetime','num']]
+        unique_together=[['scenceid','date','detailtime','num']]
