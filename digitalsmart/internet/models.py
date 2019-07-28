35833bc6f1ba8479778d2fffc35e279476111983
LianZS <2214057556@qq.com>
2019-07-28 12:10:22 +0800

    数据库录入

--- digitalsmart/internet/models.py
+++ digitalsmart/internet/models.py
@@ -4,25 +4,26 @@ from django.db import models
 # Create your models here.
 
 class MobileBrand(models.Model):
-    pid = models.SmallIntegerField(db_column="pid", verbose_name="品牌标识主键", unique=True)
+    id = models.SmallIntegerField(db_column="id", verbose_name="品牌标识主键",primary_key=True,unique=True)
     name = models.CharField(max_length=32, db_column="name", verbose_name="品牌名")
-    ddate = models.DateField(db_column="ddate", verbose_name="日期")
-    rate = models.FloatField(db_column="rate", verbose_name="品牌占有率")
+    # ddate = models.DateField(db_column="ddate", verbose_name="日期")
+    # rate = models.FloatField(db_column="rate", verbose_name="品牌占有率")
 
     class Meta:
         db_table = "mobilebrand"
 
 
-class MobileModel(models.Model):
-    pid = models.ForeignKey(to="MobileBrand", to_field="pid", on_delete=models.CASCADE, db_column="pid",
-                            verbose_name="品牌标识外键")
-    mpid = models.SmallIntegerField(db_column="mpid", verbose_name="机型标识", unique=True, db_index=True)
-    mmodel = models.CharField(max_length=32, db_column="mmodel", verbose_name="机型")
-    ddate = models.DateField(db_column="ddate", verbose_name="日期")
-    rate = models.FloatField(db_column="rate", verbose_name="机型占有率")
-
-    class Meta:
-        db_table = "mobilemodel"
+# class MobileModel(models.Model):
+#     pid = models.ForeignKey(to="MobileBrand", to_field="pid", on_delete=models.CASCADE, db_column="pid",
+#                             verbose_name="品牌标识外键")
+#     mpid = models.SmallIntegerField(db_column="mpid", verbose_name="机型标识", unique=True, db_index=True)
+#     mmodel = models.CharField(max_length=32, db_column="mmodel", verbose_name="机型")
+#     ddate = models.DateField(db_column="ddate", verbose_name="日期")
+#     rate = models.FloatField(db_column="rate", verbose_name="机型占有率")
+#     brandtype = models.CharField(max_length=8, db_column="brandtype", verbose_name="品牌分类:安卓，苹果")
+#
+#     class Meta:
+#         db_table = "mobilemodel"
 
 
 class ModelModel(models.Model):
