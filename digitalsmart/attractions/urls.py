cb0561757e382b3694a87de109675e6d855d5aef
LianZS <2214057556@qq.com>
2019-08-11 20:29:42 +0800

    评价指数完毕

--- digitalsmart/attractions/urls.py
+++ digitalsmart/attractions/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,include
 from .admin_views import Admin
 from .areainfomation import AreaInfo
 from .peopleflow import PeopleFlow
@@ -10,16 +10,20 @@ areainfo = AreaInfo()
 flow=PeopleFlow()
 comment=Comment()
 urlpatterns = {
-    path("api/getCitysByProvince", areainfo.citylist),  # 获取省份下所有城市列表
-    path("api/getRegionsByCity", areainfo.scencelist),  # 2.获取城市下所有地区列表
-    path("api/getLocation_pn_percent_new", flow.scenceflow_data),  # 实时人流接口
-    path("api/getLocation_trend_percent_new", flow.scenceflow_trend),  # 实时人流趋势
-    path("api/getLocation_search_rate", comment.search_heat),  # 地区全网搜索次数
-    path("api/getLocation_distribution_rate", flow.scence_people_distribution),  # 地区实时人口分布热力图数据
-    path("api/getLocation_geographic_bounds", areainfo.scence_geographic),  # 地区经纬度范围
+    path("api/",include([
+        path("getCitysByProvince", areainfo.citylist),  # 获取省份下所有城市列表
+        path("getRegionsByCity", areainfo.scencelist),  # 2.获取城市下所有地区列表
+        path("getLocation_pn_percent_new", flow.scenceflow_data),  # 实时人流接口
+        path("getLocation_trend_percent_new", flow.scenceflow_trend),  # 实时人流趋势
+        path("getLocation_search_rate", comment.search_heat),  # 地区全网搜索次数
+        path("getLocation_distribution_rate", flow.scence_people_distribution),  # 地区实时人口分布热力图数据
+        path("getLocation_geographic_bounds", areainfo.scence_geographic),  # 地区经纬度范围
+        path("getCommentRate", comment.get_comment_rate),  # 获取评价关键词指数
+    ])),
+
+
     path("api/upload",upload_photo),#上传照片
     path("api/getImage",get_photo_url),#获取图片链接
-    path("api/getCommentRate",comment.get_comment_rate),#获取评价关键词指数
     path("admin/provinces", admin.get_all_provinces),
     path("admin/area_cover", admin.get_cover_pic),
 
