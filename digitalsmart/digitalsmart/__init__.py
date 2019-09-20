a554ede470141349cf8a1c0043557ebce5a1ec31
LianZS <2214057556@qq.com>
2019-09-20 20:22:03 +0800

    celery报错处理

--- digitalsmart/digitalsmart/__init__.py
+++ digitalsmart/digitalsmart/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import, unicode_literals
+
 import sys
 import os
 # sys.path[0] = os.path.abspath(os.path.join(os.path.curdir, "venv/lib/python3.7/site-packages"))
@@ -6,9 +8,9 @@ import os
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 import pymysql
+# from __future__ import absolute_import, unicode_literals
+from .celeryconfig import app as share_app
 
 pymysql.install_as_MySQLdb()
-
-from .celeryconfig import app as share_app
 
 share_app.config_from_object(celeryconfig)
