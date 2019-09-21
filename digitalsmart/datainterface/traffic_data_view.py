f3da6f5441aab92e2fec029cce4fd1b42d327c2b
LianZS <2214057556@qq.com>
2019-09-22 01:13:42 +0800

    PEP8规范

--- digitalsmart/datainterface/traffic_data_view.py
+++ digitalsmart/datainterface/traffic_data_view.py
@@ -1,16 +1,12 @@
-import datetime
-import json
-import uuid
 from django.http import JsonResponse
-from django.core.cache import cache
 from traffic.models import CityInfoManager, CityTraffic, RoadTraffic, YearTraffic, RoadInfoManager, AirState
-from attractions.tool.access_control_allow_origin import Access_Control_Allow_Origin
 
 
-class CityTrafficView():
+class CityTrafficView:
 
     # http://127.0.0.1:8000/interface/api/getCitydailyIndex?cityCode=340&ddate=20190722&token=bGlhbnpvbmdzaGVuZw==
-    def daily_index(self, request):
+    @staticmethod
+    def daily_index(request):
         pid = request.GET.get("cityCode")
         ddate = request.GET.get("ddate")
         token = request.GET.get("token")
@@ -43,7 +39,8 @@ class CityTrafficView():
         return JsonResponse(response)
 
     # http://127.0.0.1:8000/interface/api/getCityRoadlist?cityCode=100&token=bGlhbnpvbmdzaGVuZw==
-    def road_list(self, request):
+    @staticmethod
+    def road_list(request):
         pid = request.GET.get("cityCode")
         token = request.GET.get("token")
         if token != "bGlhbnpvbmdzaGVuZw==":
@@ -57,7 +54,7 @@ class CityTrafficView():
             return JsonResponse({"status": 0, "code": 0, "message": "参数有误"})
 
         updateSet = RoadInfoManager.objects.filter(citypid=pid).values("up_date")
-        ##找出最早的时间，避免因为挖掘数据时出现了一个差错而导致部分未能正常录入，保证数据能完全展示给用户
+        # 找出最早的时间，避免因为挖掘数据时出现了一个差错而导致部分未能正常录入，保证数据能完全展示给用户
         up_date = sorted(updateSet, key=lambda x: x['up_date'])[0]['up_date']
         result = RoadTraffic.objects.filter(citypid=pid, up_date=up_date).values("roadname", "speed", "direction",
                                                                                  "roadpid", "rate")
@@ -70,7 +67,8 @@ class CityTrafficView():
         return JsonResponse(response)
 
     # http://127.0.0.1:8000/interface/api/getCityMonthsTraffic?cityCode=235&token=bGlhbnpvbmdzaGVuZw==
-    def yeartraffic(self, request):
+    @staticmethod
+    def yeartraffic(request):
         pid = request.GET.get("cityCode")
         token = request.GET.get("token")
         if token != "bGlhbnpvbmdzaGVuZw==":
@@ -100,7 +98,8 @@ class CityTrafficView():
         return JsonResponse(response)
 
     # http://127.0.0.1:8000/interface/api/getCityAirState?&cityCode=810000&token=bGlhbnpvbmdzaGVuZw==
-    def get_city_air(self, request):
+    @staticmethod
+    def get_city_air(request):
         # 获取城市空气状况
         pid = request.GET.get("cityCode")
         token = request.GET.get("token")
@@ -108,7 +107,7 @@ class CityTrafficView():
             return JsonResponse({"status": 0, "message": "appkey错误"})
         try:
             obj = AirState.objects.filter(citypid=pid, flag=True).order_by("-lasttime"). \
-                    values("lasttime", 'aqi', 'pm2', 'pm10', 'co', 'no2', 'o3', 'so2')[0]
+                values("lasttime", 'aqi', 'pm2', 'pm10', 'co', 'no2', 'o3', 'so2')[0]
         except IndexError:
             return JsonResponse({"status": 0, "message": "不好意思，最新数据还未采集成功"})
         lasttime = obj['lasttime']
@@ -120,18 +119,17 @@ class CityTrafficView():
         o3 = obj['o3']
         so2 = obj['so2']
         response = {
-                "cityCode": pid,
-                "lasttime": lasttime,
-                "data": {
-                    "aqi": aqi,
-                    "pm2": pm2,
-                    "pm10": pm10,
-                    "co": co,
-                    "no2": no2,
-                    "o3": o3,
-                    "so2": so2
+            "cityCode": pid,
+            "lasttime": lasttime,
+            "data": {
+                "aqi": aqi,
+                "pm2": pm2,
+                "pm10": pm10,
+                "co": co,
+                "no2": no2,
+                "o3": o3,
+                "so2": so2
 
-                }
             }
+        }
         return JsonResponse(response)
-
