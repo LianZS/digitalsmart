a4ec79f473f05c4fbc5250b019e361b044e7c35b
LianZS <2214057556@qq.com>
2019-09-21 16:52:55 +0800

    新增音乐搜索结果接口

--- digitalsmart/datainterface/urls.py
+++ digitalsmart/datainterface/urls.py
@@ -15,7 +15,9 @@ urlpatterns = {
         path("getScenceDataByTime", scence.interface_historytime_scence_data),  # 获取景区某时刻人流
         path("getScenceDataByDate", scence.interface_historydate_scence_data),  # 获取景区某天人流
         path("getScenceHeatmapDataByTime", scence.interface_hisroty_scence_distribution_data),  # 获取景区某时刻人流分布情况
-        path("getMusic", crack.get_music),  # 获取音乐
+        path("getMusic", crack.get_music),  # 搜索音乐
+        path("getMusicResult", crack.get_result_music_list),  # 搜索音乐
+
 
         path("downMusic", crack.down_music),  # 下载音乐
         path("validation", crack.identity_authentication),  # 身份认证
