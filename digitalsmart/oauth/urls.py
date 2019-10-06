d9659d8cc8eb6e844942ea9b59ae9f9988468617
LianZS <2214057556@qq.com>
2019-10-06 23:23:21 +0800

    check接口链接修改

--- digitalsmart/oauth/urls.py
+++ digitalsmart/oauth/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.yb_login, name='yb_login'),
-    path('check/', views.yb_check, name='yb_check'),
+    path('check', views.yb_check, name='yb_check'),
     # path('bind/', views.yb_bind, name='yb_bind'),
 
 ]
