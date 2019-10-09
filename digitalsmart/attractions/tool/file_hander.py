b8f7615a44a12f8a890ef48ac3a56a84c7212f0f
LianZS <2214057556@qq.com>
2019-10-09 23:57:21 +0800

    文件迭代器

--- digitalsmart/attractions/tool/file_hander.py
+++ digitalsmart/attractions/tool/file_hander.py
@@ -1,4 +1,12 @@
 class Hander_File:
-    def hander_file(self, file):
-        for c in file.chunks(chunk_size=64):
-            yield c
+    @staticmethod
+    def hander_file(file):
+        try:
+            for chunk in file.chunks(chunk_size=64):  # djiango自带的文件类型
+                yield chunk
+        except Exception:
+            while 1:  # python内置的file类型
+                chunk = file.read(64)
+                if not chunk:
+                    break
+                yield chunk
