72e7e886d5888027f2302283f3352efdf0b0ab4d
lianzongsheng <2214057556@qq.com>
2020-01-26 22:18:41 +0800

    景区地理信息优化

--- digitalsmart/attractions/areainfomation_view.py
+++ digitalsmart/attractions/areainfomation_view.py
@@ -2,124 +2,162 @@ import uuid
 from django.views.decorators.cache import cache_page
 from django.core.cache import cache
 from django.http import JsonResponse
+from django.http.request import HttpRequest
 from attractions.tool.access_control_allow_origin import Access_Control_Allow_Origin
 from attractions.models import ScenceManager
 from django.db import connection
 
 
-class AreaInfo():
+def access_control_allow_origin(httpresponse: dict) -> JsonResponse:
+    httpresponse = JsonResponse(httpresponse)
+    httpresponse["Access-Control-Allow-Origin"] = "*"
+    httpresponse["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    httpresponse["Access-Control-Max-Age"] = "1000"
+    httpresponse["Access-Control-Allow-Headers"] = "*"
+
+    return httpresponse
+
+
+class RequestMethod(object):
+    GET = 1
+    POST = 2
+
+
+def check_request_method(request: HttpRequest):
+    """
+    检查request方法
+    :param request:HttpRequest对象
+    :return:
+    """
+
+    if request.method == "GET":
+        return RequestMethod.GET
+    else:
+        return RequestMethod.POST
+
+
+class AreaInfoDetail(object):
 
     @staticmethod
-    def citylist(request):
+    def get_city_queryset(request):
         """
         景区地理基本信息--链接格式：
         http://127.0.0.1:8000/attractions/api/getCitysByProvince?province=广东省
         :param request:
         :return:
         """
-        # 城市列表
-        # if not 'User-Agent' in request.headers or len(request.COOKIES.values()) == 0:  # 反爬虫
-        #
-        #     return JsonResponse({"status": 0})
-        province = request.GET.get('province')  # 广东省
-        if province is None:
-            return JsonResponse({"status": 0, "code": 0, "message": "参数有误"})
-        response = cache.get(province)
-        if response is None:
-            result = ScenceManager.objects.filter(province=province).values("loaction", "citypid").distinct()
-            response = {"province": province, "city": list(result)}
-            cache.set(province, response, 60 * 60 * 10)
+        jsonreponse: JsonResponse = None
+        if check_request_method(request) == RequestMethod.GET:
+
+            province = request.GET.get('province')  # 广东省
+            if province is None:
+                jsonreponse = JsonResponse({"status": 0, "code": 0, "message": "参数有误"})
+            else:
+                response = cache.get(province)
+                if response is None:
+                    result = ScenceManager.objects.filter(province=province).values("loaction", "citypid").distinct()
+                    response = {"province": province, "city": list(result)}
+                    cache.set(province, response, 60 * 60 * 10)
+                jsonreponse = access_control_allow_origin(response)
         # 站点跨域请求的问题
-        return AreaInfo.deal_response(response)
+        return jsonreponse
 
     @staticmethod
-    def scencelist(request):
+    def get_scenic_queryset(request):
         """
         景区数据---flag=1的景点暂时不公开--链接格式：
         http://127.0.0.1:8000/attractions/api/getRegionsByCity?province=广东省&location=深圳市&citypid=340
         :param request:
         :return:
         """
-        # if not 'User-Agent' in request.headers or len(request.COOKIES.values()) == 0:  # 反爬虫
-        #     return JsonResponse({"status": 0})
-        province = request.GET.get("province")
-        city = request.GET.get("location")  # 深圳市
-        citypid = request.GET.get("citypid")  # 123
-        if not len(city) or not len(province) or not citypid:
-            return JsonResponse({"status": 0, "code": 0, "message": "参数有误"})
-        # 作为城市唯一缓存key---province + city + citypid
-        key = uuid.uuid5(uuid.NAMESPACE_OID, province + city + citypid)
-        response = cache.get(key)
+        jsonreponse: JsonResponse = None
+        if check_request_method(request) == RequestMethod.GET:
+            province = request.GET.get("province", None)
+            city = request.GET.get("location", None)  # 深圳市
+            citypid = request.GET.get("citypid", None)  # 123
+            if not (province and city and citypid):
+                jsonreponse = JsonResponse({"status": 0, "code": 0, "message": "参数有误"})
+            # 作为城市唯一缓存key---province + city + citypid
+            else:
+                key = uuid.uuid5(uuid.NAMESPACE_OID, province + city + citypid)
+                response = cache.get(key)
+                if response is None:
+                    result = ScenceManager.objects.filter(province=province, loaction=city, citypid=citypid,
+                                                          flag=0).values(
+                        "area",
+                        "pid",
+                        "longitude",
+                        "latitude", "type_flag")
+                    response = {"city": city, "area": list(result)}
+                    cache.set(key, response, 60 * 60 * 10)
+                jsonreponse = access_control_allow_origin(response)
 
-        if response is None:
-            result = ScenceManager.objects.filter(province=province, loaction=city, citypid=citypid, flag=0).values(
-                "area",
-                "pid",
-                "longitude",
-                "latitude", "type_flag")
-            response = {"city": city, "area": list(result)}
-            cache.set(key, response, 60 * 60 * 10)
-        # 站点跨域请求的问题
-        return AreaInfo.deal_response(response)
+            # 站点跨域请求的问题
+        return jsonreponse
 
     @staticmethod
-    def scence_geographic(request):
+    def get_scenic_geographic(request):
         """
          景区地理数据--链接格式：
          http://127.0.0.1:8000/attractions/api/getLocation_geographic_bounds?pid=1398&type_flag=1
         :param request:
         :return:
         """
+        jsonreponse: JsonResponse = None
 
-        # if not 'User-Agent' in request.headers or len(request.COOKIES.values()) == 0:  # 反爬虫
-        #     return JsonResponse({"status": 0})
-        pid = request.GET.get("pid")
-        type_flag = request.GET.get("type_flag")  # 避免同pid冲突
-        if not pid:
-            return JsonResponse({"status": 0, "code": 0, "message": "参数有误"})
-        try:
-            pid = int(pid)
-            flag = int(type_flag)
-        except Exception:
-            return JsonResponse({"status": 0, "code": 0, "message": "参数有误"})
-        # 生产该景点的唯一key
-        key = uuid.uuid5(uuid.NAMESPACE_OID, "geographic" + str(pid * 1111 + flag))
-        response = cache.get(key)
-        if response is None:
-            with connection.cursor() as cursor:
-                cursor.execute("select longitude,latitude from digitalsmart.geographic where pid=%s and flag=%s",
-                               [pid, flag])
-                rows = cursor.fetchall()
+        if check_request_method(request) == RequestMethod.GET:
 
-            response = {"bounds": rows}
-            cache.set(key, response, 60 * 60 * 10)
-        return AreaInfo.deal_response(response)
+            pid = request.GET.get("pid")
+            type_flag = request.GET.get("type_flag")  # 避免同pid冲突
+            if not (pid and type_flag):
+                jsonreponse = JsonResponse({"status": 0, "code": 0, "message": "参数有误"})
+            else:
+                try:
+                    pid = int(pid)
+                    flag = int(type_flag)
+                except Exception:
+                    jsonreponse = JsonResponse({"status": 0, "code": 0, "message": "参数有误"})
+                    return jsonreponse
+                # 生产该景点的唯一key
+                key = uuid.uuid5(uuid.NAMESPACE_OID, "geographic" + str(pid * 1111 + flag))
+                response = cache.get(key)
+                if response is None:
+                    with connection.cursor() as cursor:
+                        cursor.execute(
+                            "select longitude,latitude from digitalsmart.geographic where pid=%s and flag=%s",
+                            [pid, flag])
+                        rows = cursor.fetchall()
+
+                    response = {"bounds": rows}
+                    cache.set(key, response, 60 * 60 * 10)
+                jsonreponse = access_control_allow_origin(response)
+        else:
+            jsonreponse = JsonResponse({"status": 0, "code": 0, "message": "请求方式有误"})
+
+        return jsonreponse
 
     @staticmethod
-    def scence_map(request):
+    def get_scenic_map(request):
         """
         获取景区数据,用于绘制地图--链接格式：
         http://127.0.0.1:8000/attractions/api/getScenceInfo
         :param request:
         :return:
         """
-        #
-        # if not 'User-Agent' in request.headers or len(request.COOKIES.values()) == 0:  # 反爬虫
-        #     return JsonResponse({"status": 0})
-        key = "scence_map"
-        response = cache.get(key)
+        jsonreponse: JsonResponse = None
 
-        if response is None:
-            scence_info = ScenceManager.objects.filter(flag=0).values("area", "longitude", "latitude", "province",
-                                                                      "loaction").iterator()
-            response = {"data": list(scence_info)}
-            cache.set(key, response, 60 * 60 * 10)
+        if check_request_method(request) == RequestMethod.GET:
 
-        return AreaInfo.deal_response(response)
+            key = "scence_map"
+            response = cache.get(key)
 
-    @staticmethod
-    def deal_response(response):
+            if response is None:
+                scence_info = ScenceManager.objects.filter(flag=0).values("area", "longitude", "latitude", "province",
+                                                                          "loaction").iterator()
+                response = {"data": list(scence_info)}
+                cache.set(key, response, 60 * 60 * 10)
+            jsonreponse = access_control_allow_origin(response)
+        else:
+            jsonreponse = JsonResponse({"status": 0, "code": 0, "message": "请求方式有误"})
 
-        response = Access_Control_Allow_Origin(response)
-
-        return response
+        return jsonreponse
--- digitalsmart/attractions/urls.py
+++ digitalsmart/attractions/urls.py
@@ -1,37 +1,37 @@
 from django.urls import path, include
 from .admin_views import Admin
-from .areainfomation_view import AreaInfo
+from .areainfomation_view import AreaInfoDetail
 from .peopleflow_view import PeopleFlow
 from .comment_view import Comment
 from .views import upload_photo, get_photo_url
 
 admin = Admin()
-areainfo = AreaInfo()
+areainfo = AreaInfoDetail()
 
 flow = PeopleFlow()
 comment = Comment()
 urlpatterns = {
     path("api/", include([
-        path("getCitysByProvince", areainfo.citylist),  # 获取省份下所有城市列表
-        path("getRegionsByCity", areainfo.scencelist),  # 2.获取城市下所有地区列表
+        path("getCitysByProvince", AreaInfoDetail.get_city_queryset),  # 获取省份下所有城市列表
+        path("getRegionsByCity", AreaInfoDetail.get_scenic_queryset),  # 2.获取城市下所有地区列表
         path("getLocation_pn_percent_new", flow.scenceflow_data),  # 实时人流接口
         path("getLocation_trend_percent_new", flow.scenceflow_trend),  # 实时人流趋势
         path("getLocation_search_rate", comment.search_heat),  # 地区全网搜索次数
         path("getLocation_distribution_rate", flow.scence_people_distribution),  # 地区实时人口分布热力图数据
-        path("getLocation_geographic_bounds", areainfo.scence_geographic),  # 地区经纬度范围
+        path("getLocation_geographic_bounds", AreaInfoDetail.get_scenic_geographic),  # 地区经纬度范围
         path("getCommentRate", comment.get_comment_rate),  # 获取评价关键词指数
         path("getComment", comment.get_comment),  # 获取评价关键词指数
         path("getState", comment.get_state),  # 获取景区状况
         path("upload", upload_photo),  # 上传照片
         path("getImage", get_photo_url),  # 获取图片链接
-        path("getScenceInfo", areainfo.scence_map),  # 获取景区数据
+        path("getScenceInfo", AreaInfoDetail.get_scenic_map),  # 获取景区数据
 
     ])),
 
     path("admin/", include([
         path("provinces", admin.get_all_provinces),  # 管理员景区管理的省份界面数据
         path("area_cover", admin.get_cover_pic),  # 管理员景区管理的城市界面图片
-        path("uploadCommentRate",admin.up_comment_rate),#更新评价指数
+        path("uploadCommentRate", admin.up_comment_rate),  # 更新评价指数
         path("uploadComment", admin.up_comment),  # 更新评价
 
     ])),
