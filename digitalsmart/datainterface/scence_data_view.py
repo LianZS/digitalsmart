b9b711ce863764c481ac80667a097bff580d56e4
lianzongsheng <2214057556@qq.com>
2020-01-28 21:17:54 +0800

    景区接口优化

--- digitalsmart/datainterface/scence_data_view.py
+++ digitalsmart/datainterface/scence_data_view.py
@@ -1,55 +1,86 @@
 import re
 import datetime
 
-from django.db import connection
 from django.http import JsonResponse
-
+from django.core.exceptions import ObjectDoesNotExist
 from attractions.models import TableManager, ScenceManager
 from .models import WeatherDB
 from datainterface.tasks import NetWorker
-from attractions.model_choice import ModelChoice
+from attractions.tool.select_historyflown import get_historyscenceflow_class
+from attractions.tool.processing_request import check_request_method, conversion_args_type, get_request_args, \
+    RequestMethod
+
+from attractions.tool.processing_response import access_control_allow_origin
+
+
+def check_paramer(request, flag=1):
+    """参数检查
+    flag为1时表示要检查ttime格式，0表示不需要
+    """
+    pid = request.GET.get("pid")
+    token: str = request.GET.get("token")  # 密钥
+    ddate = request.GET.get("ddate")  # 日期
+    ttime: str = request.GET.get("ttime")  # 时间
+
+    if not (pid and token and ddate):
+        return None, None, None, None
+    try:
+        ddate: int = int(ddate)
+        pid: int = int(pid)
+        if flag:
+
+            match_result = re.match("\d{2}:\d{2}:00", ttime)
+            if not match_result:
+                return None, None, None, None
+
+    except Exception:
+        return None, None, None, None
+    return pid, ddate, ttime, token
 
 
 class ScenceData(object):
 
-    # Create your views here.
-
-    def interface_historytime_scence_data(self, request):
+    @staticmethod
+    def interface_get_historytime_scence_queryset(request):
         """
         获取历史某个具体时刻的人流量--链接格式：
-         http://127.0.0.1:8000/interface/api/getScenceDataByTime?pid=6&ddate=20170916&
-         ttime=22:00:00&flag=0&token=bGlhbnpvbmdzaGVuZw==
+         http://127.0.0.1:8000/interface/api/getScenceDataByTime?pid=6&ddate=20170916&ttime=22:00:00&flag=0&token=bGlhbnpvbmdzaGVuZw==
         :param request:
         :return:
 
         """
-        flag = request.GET.get("flag")
+        err_msg = {"status": 0, "message": "错误"}
 
-        pid, ddate, ttime, token = self.check_paramer(request)
-        if token != "bGlhbnpvbmdzaGVuZw==":
-            return JsonResponse({"status": 0, "message": "appkey错误"})
-        if not (pid and ddate and ttime and token and flag):
-            return JsonResponse({"status": 0, "message": "参数格式有误"})
+        if check_request_method(request) == RequestMethod.GET:
+            pid, ddate, ttime, token, flag = get_request_args(request, 'pid', 'ddate', 'ttime', 'token', 'flag')
+            pid, ddate, ttime, token, flag = conversion_args_type(
+                {pid: int, ddate: int, ttime: str, token: str, flag: int})
+            if token != "bGlhbnpvbmdzaGVuZw==" or not (
+                    pid and ddate and re.match("\d{2}:\d{2}:00", ttime) and token and isinstance(flag, int)):
+                response = err_msg
+            else:
+                try:
+                    obj = TableManager.objects.get(pid=pid, flag=0)  # 查询目标所在的表位置
+                except ObjectDoesNotExist:
+                    return access_control_allow_origin({"status": 0, "message": "无目标数据"})
 
-        try:
-            obj = TableManager.objects.get(pid=pid, flag=0)  # 查询目标所在的表位置
+                area = obj.area
+                # 查询历史数据
+                table_id: int = obj.table_id  # 表地址
+                # 下面之所以不格式化字符串，是预防注入
+                historyscenceflow = get_historyscenceflow_class(table_id)
+                result = historyscenceflow.objects.filter(ddate=ddate, ttime=ttime).values('num')
+                num = None
+                for item in result:
+                    num = item['num']
+                response = {"num": num, "pid": pid, "area": area, "date": ddate, "ttime": ttime}
+        else:
+            response = err_msg
+        jsonresponse = access_control_allow_origin(response)
+        return jsonresponse
 
-        except Exception:
-            return JsonResponse({"status": 0, "message": "无目标数据"})
-
-        area = obj.area
-        # 查询历史数据
-        table_id: int = obj.table_id  # 表为止
-        # 下面之所以不格式化字符串，是预防注入
-        historyscenceflow = ModelChoice.historyscenceflow(table_id)
-        result = historyscenceflow.objects.filter(ddate=ddate, ttime=ttime).values('num')
-        num = None
-        for item in result:
-            num = item['num']
-
-        return JsonResponse({"num": num, "pid": pid, "area": area, "date": ddate, "ttime": ttime})
-
-    def interface_historydate_scence_data(self, request):
+    @staticmethod
+    def interface_get_historydate_scence_queryset(request):
         """
         查询某天人流情况---链接格式：
         http://127.0.0.1:8001/interface/api/getScenceDataByDate?pid=6&ddate=20190921&
@@ -57,103 +88,93 @@ class ScenceData(object):
         :param request:
         :return:
         """
-        flag = request.GET.get("flag")
+        err_msg = {"status": 0, "message": "错误"}
 
-        pid, ddate, ttime, token = self.check_paramer(request, 0)  # ttime默认为None
-        if token != "bGlhbnpvbmdzaGVuZw==":
-            return JsonResponse({"status": 0, "message": "appkey错误"})
-        if not (pid and ddate and token and flag):
-            return JsonResponse({"status": 0, "message": "参数格式有误"})
+        if check_request_method(request) == RequestMethod.GET:
+            pid, ddate, token, flag = get_request_args(request, 'pid', 'ddate', 'token', 'flag')
+            pid, ddate, token, flag = conversion_args_type(
+                {pid: int, ddate: int, token: str, flag: int})
+            if token != "bGlhbnpvbmdzaGVuZw==" or not (pid and ddate and isinstance(flag, int)):
+                response = err_msg
 
-        today: int = int(str(datetime.datetime.today().date()).replace("-", ""))
+            else:
 
-        try:
-            obj = TableManager.objects.get(pid=pid, flag=0)  # 查询目标所在的表位置
+                try:
+                    obj = TableManager.objects.get(pid=pid, flag=flag)  # 查询目标所在的表位置
 
-        except Exception:
-            return JsonResponse({"status": 0, "code": 0, "message": "参数有误"})
+                except ObjectDoesNotExist:
+                    return access_control_allow_origin({"status": 0, "message": "无目标数据"})
 
-        area = obj.area
+                area = obj.area
 
-        # 查询历史数据
-        table_id: int = obj.table_id  # 表 地址
-        # 下面之所以不格式化字符串，是预防注入
-        historyscenceflow = ModelChoice.historyscenceflow(table_id)
-        result = historyscenceflow.objects.filter(ddate=ddate).values('ttime', 'num')
-        rows = list()
-        for item in result:
-            ttime = item['ttime']
-            num = item['num']
-            rows.append([ttime, num])
-
-        return JsonResponse({"area": area, "pid": pid, "datalist": rows, "date": ddate})
+                # 查询历史数据
+                table_id: int = obj.table_id  # 表 地址
+                # 下面之所以不格式化字符串，是预防注入
+                historyscenceflow = get_historyscenceflow_class(table_id)
+                result = historyscenceflow.objects.filter(ddate=ddate).values('ttime', 'num')
+                history_scenic_queryset = list()
+                for item in result:
+                    ttime = item['ttime']
+                    num = item['num']
+                    history_scenic_queryset.append([ttime, num])
+                response = {"area": area, "pid": pid, "datalist": history_scenic_queryset, "date": ddate}
+        else:
+            response = err_msg
+        jsonresponse = access_control_allow_origin(response)
+        return jsonresponse
 
     @staticmethod
-    def check_paramer(request, flag=1):
-        """参数检查
-        flag为1时表示要检查ttime格式，0表示不需要
-        """
-        pid = request.GET.get("pid")
-        token: str = request.GET.get("token")  # 密钥
-        ddate = request.GET.get("ddate")  # 日期
-        ttime: str = request.GET.get("ttime")  # 时间
-
-        if not (pid and token and ddate):
-            return None, None, None, None
-        try:
-            ddate: int = int(ddate)
-            pid: int = int(pid)
-            if flag:
-
-                match_result = re.match("\d{2}:\d{2}:00", ttime)
-                if not match_result:
-                    return None, None, None, None
-
-        except Exception:
-            return None, None, None, None
-        return pid, ddate, ttime, token
-
-    # http://127.0.0.1:8000/interface/api/getScenceHeatmapDataByTime?pid=6&ddate=20170121&ttime=12:00:00&flag =0&token=bGlhbnpvbmdzaGVuZw==
-    def interface_hisroty_scence_distribution_data(self, request):
+    def interface_get_hisroty_distribution_queryset(request):
         """
         获取历史景区人流分布数据
+     http://127.0.0.1:8000/interface/api/getScenceHeatmapDataByTime?pid=6&ddate=20170121&ttime=12:00:00&flag =0&token=bGlhbnpvbmdzaGVuZw==
+
         :param request:
         :return:
         """
-        flag = request.GET.get("flag")
+        err_msg = {"status": 0, "message": "错误"}
 
-        pid, ddate, ttime, token = self.check_paramer(request)
-        if token != "bGlhbnpvbmdzaGVuZw==":
-            return JsonResponse({"status": 0, "message": "appkey错误"})
-        year, month, day = str(ddate)[0:4], str(ddate)[4:6], str(ddate)[6:]
-        ddate = '-'.join([year, month, day])
+        if check_request_method(request) == RequestMethod.GET:
+            pid, ddate, ttime, token, flag = get_request_args(request, 'pid', 'ddate', 'ttime', 'token', 'flag')
+            pid, ddate, ttime, token, flag = conversion_args_type(
+                {pid: int, ddate: int, ttime: str, token: str, flag: int})
+            if token != "bGlhbnpvbmdzaGVuZw==" or not (
+                    pid and ddate and re.match("\d{2}:\d{2}:00", ttime) and isinstance(flag, int)):
+                response = err_msg
+            else:
+                year, month, day = str(ddate)[0:4], str(ddate)[4:6], str(ddate)[6:]
+                ddate = '-'.join([year, month, day])
+                try:
+                    obj = ScenceManager.objects.get(pid=pid, flag=0)  # 检查是否存在
+                except:
+                    return access_control_allow_origin(err_msg)
+                area = obj.area
+                longitude = obj.longitude  # 中心经度
+                latitude = obj.latitude  # 中心维度
+                data = NetWorker().get_scence_distribution_data(pid, ddate, ttime)
+                if data == {}:
+                    return access_control_allow_origin({"status": 0, "message": "本次请求失败，请重试"})
+                response = {"pid": pid, "area": area, "data": data, "longitude": longitude, "latitude": latitude,
+                            "multiple": 10000}
+        else:
+            response = err_msg
+        jsonresponse = access_control_allow_origin(response)
+        return jsonresponse
 
-        try:
-            obj = ScenceManager.objects.get(pid=pid, flag=0)  # 检查是否存在
-        except Exception:
-            return JsonResponse({"status": 0, "code": 0, "message": "参数有误"})
-        area = obj.area
+    @staticmethod
+    def interface_get_hisroty_weather(request):
+        err_msg = {"status": 0, "message": "错误"}
 
-        longitude = obj.longitude  # 中心经度
-        latitude = obj.latitude  # 中心维度
-        data = NetWorker().get_scence_distribution_data(pid, ddate, ttime)
-        if data == {}:
-            return JsonResponse({"status": 0, "message": "本次请求失败，请重试"})
-        response = {"pid": pid, "area": area, "data": data, "longitude": longitude, "latitude": latitude,
-                    "multiple": 10000}
-        return JsonResponse(response)
-
-    def interface_hisroty_weather(self, request):
-        pid = request.GET.get("pid")
-        ddate = request.GET.get("ddate")
-        if not (pid and ddate):
-            return JsonResponse({"status": 0, "code": 0, "message": "参数有误"})
-        try:
-            pid = int(pid)
-            ddate = int(ddate)
-        except Exception:
-            return JsonResponse({"status": 0, "code": 0, "message": "参数有误"})
-        result = WeatherDB.objects.filter(pid=pid, ddate=ddate).values("ddate", "weatherstate", "template",
-                                                                       "wind").iterator()
-        response = {"data": list(result)}
-        return JsonResponse(response)
+        if check_request_method(request) == RequestMethod.GET:
+            pid, ddate, token = get_request_args(request, 'pid', 'ddate', 'token')
+            pid, ddate = conversion_args_type({pid: int, ddate: int})
+            if token != "bGlhbnpvbmdzaGVuZw==" or not (pid and ddate):
+                response = err_msg
+            else:
+                result = WeatherDB.objects.filter(pid=pid, ddate=ddate).values("ddate", "weatherstate", "template",
+                                                                               "wind").iterator()
+                response = {"data": list(result)}
+        else:
+            response = err_msg
+        jsonresponse = access_control_allow_origin(response)
+        return jsonresponse
