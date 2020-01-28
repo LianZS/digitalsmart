cd4a86b7cea83af3f81024655911124d6ae8a4a5
lianzongsheng <2214057556@qq.com>
2020-01-28 20:36:31 +0800

    路由优化

--- digitalsmart/attractions/urls.py
+++ digitalsmart/attractions/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path, include
 from .admin_views import Admin
 from .areainfomation_view import AreaInfoDetail
-from .peopleflow_view import PeopleFlow
+from .peopleflow_view import ScenicPeopleFlowDetail
 from .comment_view import CommentDetail
 from .views import upload_photo, get_photo_url
 
 admin = Admin()
 
-flow = PeopleFlow()
 urlpatterns = {
     path("api/", include([
         path("getCitysByProvince", AreaInfoDetail.get_city_queryset),  # 获取省份下所有城市列表
@@ -15,10 +14,10 @@ urlpatterns = {
         path("getLocation_geographic_bounds", AreaInfoDetail.get_scenic_geographic),  # 地区经纬度范围
         path("getScenceInfo", AreaInfoDetail.get_scenic_map),  # 获取景区数据
 
-        path("getLocation_pn_percent_new", flow.get_scenicflow_data_queryset),  # 实时人流接口
-        path("getLocation_trend_percent_new", flow.get_scenicflow_trend_queryset),  # 实时人流趋势
+        path("getLocation_pn_percent_new", ScenicPeopleFlowDetail.get_scenicflow_data_queryset),  # 实时人流接口
+        path("getLocation_trend_percent_new", ScenicPeopleFlowDetail.get_scenicflow_trend_queryset),  # 实时人流趋势
         path("getLocation_search_rate", CommentDetail.get_search_heat_queryset),  # 地区全网搜索次数
-        path("getLocation_distribution_rate", flow.scence_people_distribution),  # 地区实时人口分布热力图数据
+        path("getLocation_distribution_rate", ScenicPeopleFlowDetail.scence_people_distribution),  # 地区实时人口分布热力图数据
         path("getCommentRate", CommentDetail.get_comment_rate_queryset),  # 获取评价关键词指数
         path("getComment", CommentDetail.get_comment_queryset),  # 获取评价关键词指数
         path("getState", CommentDetail.get_scenic_state_queryset),  # 获取景区状况
